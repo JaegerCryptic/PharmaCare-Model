3ff5ad4207b2f2f272bd5931fb2251155058edda
Kyle <39253934+JaegerCryptic@users.noreply.github.com>
2023-03-27 17:37:42 +1000

    Final

---
+++ PharmaCare/PharmaCare/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmaCare.Models
+{
+    public class Patient
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int RoomNumber { get; set; }
+        public string WingNumber { get; set; }
+        public string FloorNumber { get; set; }
+    }
+}
---
+++ PharmaCare/PharmaCare/Models/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmaCare.Models
+{
+    public class Staff
+    {
+        public int StaffID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
